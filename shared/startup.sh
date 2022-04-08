5f1e88a1bcaf2ac15bacfddefc1ad8844caff0e5
Davide De Tommaso <dtmdvd@gmail.com>
2022-04-08 17:58:57 +0200

    fixing pyicub ver

--- shared/startup.sh
+++ shared/startup.sh
@@ -5,7 +5,7 @@ source ${ROBOTOLOGY_SUPERBUILD_INSTALL_DIR}/share/robotology-superbuild/setup.sh
 YARP_FORWARD_LOG_ENABLE=0 yarpserver --write --ip $ICUBSRV_IP &
 sleep 1
 
-if !$ICUB_SIMULATION; then
+if ! $ICUB_SIMULATION ; then
   sshpass -p $ICUB_PSW ssh -o StrictHostKeyChecking=no $ICUB_USER@$ICUB_IP "yarprun --server /"$ICUB_HOST" --log &" &
 fi
 
@@ -13,7 +13,7 @@ yarprun --server /$ICUBSRV_HOST --log &
 
 yarpmanager --apppath ${ICUB_APPS}
 
-if !$ICUB_SIMULATION; then
+if ! $ICUB_SIMULATION ; then
   sshpass -p $ICUB_PSW ssh -o StrictHostKeyChecking=no $ICUB_USER@$ICUB_IP "killall -9 yarprun"
   sshpass -p $ICUB_PSW ssh -o StrictHostKeyChecking=no $ICUB_USER@$ICUB_IP "killall -9 yarpdev"
 fi
